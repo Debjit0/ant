7050dd194dc79069702bcb38421ba22eed14d70b
unknown <upworksaswata@gmail.com>
2023-07-23 13:39:39 +0530

    hi

---
+++ lib/screens/home_screen/home_screen.dart
@@ -0,0 +1,38 @@
+import 'package:ant/theme/app_theme.dart';
+import 'package:ant/theme/constants/project_colors.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(16),
+              ),
+              child: const Text('Home',
+                  style: TextStyle(
+                    fontFamily: AppTheme.buttonTextFamily,
+                    fontSize: AppTheme.buttonFontSizeLarge,
+                    fontWeight: AppTheme.buttonFontWeight,
+                  )),
+            ),
+            FilledButton(
+              onPressed: () {},
+              child: Text('Home'),
+            ),
+            FilledButton.tonal(onPressed: () {}, child: Text('Home')),
+            OutlinedButton(onPressed: () {}, child: Text('Home')),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,14 @@
+import 'package:ant/theme/constants/project_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  Color primaryColor = cRed;
+  Color secondaryColor = cYellow;
+  Color tertiaryColor = cBlack;
+
+  static const buttonTextFamily = 'Raleway';
+  static const double buttonFontSizeLarge = 20;
+  static const double buttonFontSizeMedium = 16;
+  static const double buttonFontSize = 12;
+  static const buttonFontWeight = FontWeight.w400;
+}
