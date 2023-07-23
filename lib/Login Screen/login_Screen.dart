87610fb8a4e43d4f3ca18a3727686294380c2984
unknown <upworksaswata@gmail.com>
2023-07-23 23:10:04 +0530

    hi

--- lib/Login Screen/login_Screen.dart
+++ lib/Login Screen/login_Screen.dart
@@ -13,80 +13,124 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset("assets/images/login.png"),
-                Form(
-                  child: SizedBox(
-                    // height: 60,
-                    width: MediaQuery.of(context).size.width - 40,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        SizedBox(
-                            height: 60,
-                            width: 90,
-                            child: ElevatedButton(
-                                style: FilledButton.styleFrom(
-                                    //backgroundColor: Colors.black87,
-                                    foregroundColor: Colors.black87),
-                                onPressed: () {},
-                                child: const Text("+91"))),
-                        SizedBox(
-                          // height: 60,
-                          width: (MediaQuery.of(context).size.width - 40) -
-                              90 -
-                              16,
-                          child: TextFormField(
-                            autovalidateMode:
-                                AutovalidateMode.onUserInteraction,
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter a phone number';
-                              } else if (value.length < 7 &&
-                                  value.length > 15) {
-                                return 'PLease enter a valid phone number';
-                              }
-                            },
-                            keyboardType: TextInputType.number,
-                            textAlign: TextAlign.center,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(100),
-                              ),
-                              hintText: 'Enter Your mobile number',
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                SizedBox(
-                    width: double.infinity,
-                    height: 60,
-                    child: FilledButton(
-                        onPressed: () {
-
-                          
-                        }, child: const Text("Get OTP")))
-              ],
+        body: Stack(
+          children: [
+            SizedBox(
+              height: height,
+              width: width,
             ),
-          ),
+            SizedBox(
+              height: height,
+              width: width,
+              child: Image.asset(
+                'assets/images/background.jpg',
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            Container(
+              height: height,
+              width: width,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.transparent,
+                    Colors.black87,
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  stops: [.1, 1],
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: height * .05,
+              child: Container(
+                height: 300,
+                width: width,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // Image.asset("assets/images/login.png"),
+                    Form(
+                      child: SizedBox(
+                        // height: 60,
+                        width: MediaQuery.of(context).size.width - 40,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            SizedBox(
+                                height: 60,
+                                width: 90,
+                                child: FilledButton.tonal(
+                                    onPressed: () {},
+                                    child: const Text("+91"))),
+                            SizedBox(
+                              width: (MediaQuery.of(context).size.width - 40) -
+                                  90 -
+                                  16,
+                              child: TextFormField(
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                ),
+                                autovalidateMode:
+                                    AutovalidateMode.onUserInteraction,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter a phone number';
+                                  } else if (value.length < 7 ||
+                                      value.length > 15) {
+                                    return 'PLease enter a valid phone number';
+                                  }
+                                },
+                                keyboardType: TextInputType.number,
+                                textAlign: TextAlign.center,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(100),
+                                    borderSide: BorderSide(),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(100),
+                                    borderSide: BorderSide(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .surfaceVariant,
+                                    ),
+                                  ),
+                                  hintText: 'Enter Your mobile number',
+                                  hintStyle:
+                                      const TextStyle(color: Colors.white70),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: FilledButton.tonal(
+                            onPressed: () {}, child: const Text("Get OTP")))
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: cRed),
+        colorScheme: ColorScheme.fromSeed(seedColor: cYellow),
         useMaterial3: true,
       ),
       home: LoginScreen(),
