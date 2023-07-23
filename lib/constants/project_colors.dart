4b0bef2cb33c1380f63a0fedf204dcb72c8ef9a4
Debjit Sarkar <68617086+Debjit0@users.noreply.github.com>
2023-07-23 09:01:29 +0530

    Create project_colors.dart

---
+++ lib/constants/project_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color cYellow = Color.fromRGBO(255, 215, 0, 1);
+Color cRed = Color.fromRGBO(211, 0, 42, 1);
+Color cBlack = Color.fromRGBO(34, 31, 31, 1);
