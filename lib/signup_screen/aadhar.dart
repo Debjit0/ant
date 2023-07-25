f6167e0357976a97f987ede515d9f6150e600748
Debjit Sarkar <68617086+Debjit0@users.noreply.github.com>
2023-07-25 21:28:24 +0530

    upload data in firebase

--- lib/signup_screen/aadhar.dart
+++ lib/signup_screen/aadhar.dart
@@ -1,11 +1,15 @@
 import 'dart:io';
 
+import 'package:ant/services/firebase_services/upload_aadhar_image.dart';
 import 'package:ant/signup_screen/utils/pick_image.dart';
+import 'package:ant/view_models/auth_provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Aadhar extends StatefulWidget {
   const Aadhar({super.key});
-
+  
   @override
   State<Aadhar> createState() => _AadharState();
 }
@@ -13,8 +17,14 @@ class Aadhar extends StatefulWidget {
 class _AadharState extends State<Aadhar> {
   String aadharFront = "";
   String aadharBack = "";
+  
+  
   @override
   Widget build(BuildContext context) {
+    var _authProvider = Provider.of<AuthProvider>(context);
+    print(_authProvider.firstName);
+    print(_authProvider.lastName);
+    print(_authProvider.email);
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -93,7 +103,10 @@ class _AadharState extends State<Aadhar> {
                     SizedBox(
                       height: 50,
                       child: FilledButton.tonal(
-                        onPressed: () async {},
+                        onPressed: () async {
+                          final uid =FirebaseAuth.instance.currentUser!.uid;
+                          UploadAadharProvider().addAadhar(uid: uid,aadharFront: File(aadharFront),aadharBack: File(aadharBack),firstName: _authProvider.firstName, lastName: _authProvider.lastName, email: _authProvider.email);
+                        },
                         child: const Row(
                           mainAxisSize: MainAxisSize.min,
                           mainAxisAlignment: MainAxisAlignment.center,
@@ -115,4 +128,7 @@ class _AadharState extends State<Aadhar> {
       ),
     );
   }
+
+
+  
 }
